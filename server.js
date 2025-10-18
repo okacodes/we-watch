fb5dfe0e23013fb4a9746bed74cdbaa6c7731cc7
nospecdev <nospecdev@gmail.com>
2025-10-17 20:29:44 -0500

    Making sure I'm up to date on

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 // const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const PORT = 3000
 
+app.use(cors);
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Fuckf23f32 man');
 })
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log('what what huh whatttt');
+    console.log(`Server listening on ${PORT}`);
 })
