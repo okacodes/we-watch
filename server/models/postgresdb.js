c0741300326effd1e2d5cc473df0ca8be0c4b515
Logan Lyons <admin@loganlyons.me>
2025-10-19 02:42:53 -0700

    got the database to connect, need to work out structure for inserting data

--- server/models/postgresdb.js
+++ server/models/postgresdb.js
@@ -1,12 +1,23 @@
-const pg = require('pg');
-const Pool = pg;
+require('dotenv').config({path: '../.env'})
+const pg = require('pg')
+const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.USER,
+    user: process.env.DBUSER,
+    password: process.env.PASSWORD,
     host: process.env.HOST,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
-    password: process.env.DBPASSWORD
+    port: process.env.DBPORT,
+    database: process.env.DATABASE
 })
 
-console.log(await pool.query('SELECT NOW()'))
+async function what(){
+    console.log('0')
+    console.log(process.env.PASSWORD)
+    const result = await pool.query('SELECT * from users')
+    console.log('1')
+    console.log(result)
+    console.log('2')
+}
+// what()
+
+module.exports = 'postgresdb'
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 // const cors = require('cors');
-const db = require('./models/postgresdb.js')
+const db = require('./models/postgresdb')
+// const db = import('./models/postgresdb')
 // const authRoutes = require('./app/route/auth.routes.js')
 // const userRoutes = require('./app/route/user.routes.js')
 // const mongoose = require('mongoose');
-// const PORT = 3000
+const PORT = 3000
 // require('dotenv').config();
 
 const app = express();
@@ -13,9 +14,22 @@ const app = express();
 //     origin: "http://localhost:8081",
 // }
 
+// app.use(cors(corsOptions));
+
 app.use(express.json());
 
-app.use(cors);
+app.use(express.urlencoded({ extended: true }));
+
+
+// const pool = new Pool({
+//     user: process.env.USER,
+//     host: process.env.HOST,
+//     database: process.env.DATABASE,
+//     password: process.env.DBPASSWORD
+// })
+
+
+// app.use(cors);
 
 // Get Request
 app.get('/', (req, res) => {
